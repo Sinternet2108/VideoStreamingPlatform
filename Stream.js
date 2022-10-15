e4ea9093577ed59801834428ceafde7f7a91b8bb
Sinternet <sindhurvshabaraya2318@gmail.com>
2022-10-16 01:39:32 +0530

    VideoStreamer

--- Stream.js
+++ Stream.js
@@ -1,4 +1,3 @@
-let i=0;
 let searchElement = document.querySelector(".searchIcon svg");
 let searchInputElement = document.querySelector(".searchBox");
 let profileIconElement = document.querySelector(".profileIcon svg");
@@ -6,6 +5,11 @@ let arrowIconElement = document.querySelector(".profileIcon img");
 let profileDiv = document.querySelector(".profileContent");
 let buttonElement = document.getElementsByClassName("flexBox");
 let arrowElement = document.querySelectorAll(".directionArrow");
+let mainContentDiv = document.querySelector(".mainStartVideo");
+let kantaraImage = document.querySelector(".kantara");
+let arrowRightSlider = document.querySelector(".moviesSlider svg");
+let movieSlider = document.querySelector(".moviesCollection");
+let arrowLeftSlider = document.querySelector(".leftArrowSlider");
 
 searchElement.addEventListener("click",expandSearch);
 
@@ -34,3 +38,17 @@ for(let i=0;i<buttonElement.length;i++)
         arrowElement[i].style.setProperty("display","none")
     })
 }
+
+arrowRightSlider.addEventListener("click",()=>{
+    movieSlider.style.setProperty("transform","translateX(-100vw)");
+    arrowRightSlider.style.setProperty("display","none");
+    arrowLeftSlider.style.setProperty("display","block");
+})
+
+arrowLeftSlider.addEventListener("click",()=>{
+    movieSlider.style.setProperty("transform","translateX(0vw)");
+    arrowLeftSlider.style.setProperty("display","none");
+    arrowRightSlider.style.setProperty("display","block");
+})
+
+
