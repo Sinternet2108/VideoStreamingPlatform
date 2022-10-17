4581b63a7a799e6e5d0d2f510c0adf10e45dfa50
Sinternet <sindhurvshabaraya2318@gmail.com>
2022-10-18 00:00:21 +0530

    TrailerVersion

--- Stream.js
+++ Stream.js
@@ -1,4 +1,4 @@
-window.onbeforeunload = function() {
+window.onbeforeunload = function () {
     return "Are You Sure? You will lead to Home Page...";
 }
 
@@ -22,62 +22,57 @@ let backArrowDiv = document.querySelector(".Backarrow");
 let backArrow = document.querySelector(".Backarrow svg");
 let backWritten = document.querySelector(".label");
 
-searchElement.addEventListener("click",expandSearch);
+searchElement.addEventListener("click", expandSearch);
 
-function expandSearch()
-{
+function expandSearch() {
     searchElement.remove();
-    searchInputElement.style.setProperty("display","block");
+    searchInputElement.style.setProperty("display", "block");
 }
 
-profileIconElement.addEventListener("click",addContent);
-arrowIconElement.addEventListener("click",addContent);
+profileIconElement.addEventListener("click", addContent);
+arrowIconElement.addEventListener("click", addContent);
 
-function addContent()
-{
+function addContent() {
     arrowIconElement.classList.toggle("rotateArrow");
     profileDiv.classList.toggle("showContent");
 }
 
-for(let i=0;i<buttonElement.length;i++)
-{
-    buttonElement[i].addEventListener("mouseover",()=>{
-        arrowElement[i].style.setProperty("display","block")
+for (let i = 0; i < buttonElement.length; i++) {
+    buttonElement[i].addEventListener("mouseover", () => {
+        arrowElement[i].style.setProperty("display", "block")
     })
 
-    buttonElement[i].addEventListener("mouseout",()=>{
-        arrowElement[i].style.setProperty("display","none")
+    buttonElement[i].addEventListener("mouseout", () => {
+        arrowElement[i].style.setProperty("display", "none")
     })
 }
 
-for(let i=0;i<movieSlider.length;i++)
-{
-    arrowRightSlider[i].addEventListener("click",()=>{
-        movieSlider[i].style.setProperty("transform","translateX(-100vw)");
-        arrowRightSlider[i].style.setProperty("display","none");
-        arrowLeftSlider[i].style.setProperty("display","block");
+for (let i = 0; i < movieSlider.length; i++) {
+    arrowRightSlider[i].addEventListener("click", () => {
+        movieSlider[i].style.setProperty("transform", "translateX(-100vw)");
+        arrowRightSlider[i].style.setProperty("display", "none");
+        arrowLeftSlider[i].style.setProperty("display", "block");
     })
-    
-    arrowLeftSlider[i].addEventListener("click",()=>{
-        movieSlider[i].style.setProperty("transform","translateX(0vw)");
-        arrowLeftSlider[i].style.setProperty("display","none");
-        arrowRightSlider[i].style.setProperty("display","block");
+
+    arrowLeftSlider[i].addEventListener("click", () => {
+        movieSlider[i].style.setProperty("transform", "translateX(0vw)");
+        arrowLeftSlider[i].style.setProperty("display", "none");
+        arrowRightSlider[i].style.setProperty("display", "block");
     })
 }
 
-trailerElement.addEventListener("click",()=>{
-    videoPlayer.style.setProperty("display","block");
-    mainContent.style.setProperty("display","none");
-    headerContent.style.setProperty("display","none");
-    backArrowDiv.style.setProperty("display","block");
+trailerElement.addEventListener("click", () => {
+    videoPlayer.style.setProperty("display", "block");
+    mainContent.style.setProperty("display", "none");
+    headerContent.style.setProperty("display", "none");
+    backArrowDiv.style.setProperty("display", "block");
 });
 
-backArrow.addEventListener("click",backFunction);
+backArrow.addEventListener("click", backFunction);
 
-function backFunction()
-{
-    videoPlayer.style.setProperty("display","none");
-    mainContent.style.setProperty("display","block");
-    headerContent.style.setProperty("display","flex");
-    backArrowDiv.style.setProperty("display","none");
+function backFunction() {
+    videoPlayer.style.setProperty("display", "none");
+    mainContent.style.setProperty("display", "block");
+    headerContent.style.setProperty("display", "flex");
+    backArrowDiv.style.setProperty("display", "none");
 }
